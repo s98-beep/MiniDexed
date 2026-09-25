bdffc9a376565969536885c49dcda9cfa2a3ce06
s98-beep <shop81.etc@gmail.com>
2026-09-26 01:37:52 +0900

    Update submod.sh

--- submod.sh
+++ submod.sh
@@ -15,7 +15,7 @@ git checkout -f --recurse-submodules b42d060
 cd -
 
 # Apply CME UF5/UF6/UF7/UF8 USB-MIDI compatibility patch
-git -C circle-stdlib/libs/circle apply "$PWD/cme-uf-usbmidi.patch"
+python3 "$PWD/apply_cme_uf.py"
 #cd circle-stdlib/libs/circle-newlib
 #git checkout develop
 #cd -
